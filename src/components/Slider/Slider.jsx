fae20379bc6626ecb34b0f67a0e173ed36a4422e
vladeskovich <vladeskovich@gmail.com>
2020-11-08 14:01:43 +0300

    add description

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import ew from './1.jpg';
 import Image from '../Image';
-
+import Typography from '../Typography';
 import styles from './Slider.scss';
 
 const Slider = ({
@@ -11,12 +10,24 @@ const Slider = ({
   onClick,
 }) => {
   const classes = classNames(styles.slider, className);
+  const baseUrl = 'http://image.tmdb.org/t/p/w500/';
+  const test = [
+    { src: `${baseUrl}pci1ArYW7oJ2eyTo2NMYEKHHiCP.jpg`, title: 'Инферно Габриэля 2' },
+    { src: `${baseUrl}riAooJrFvVhotyaOgoI0WR7okSe.jpg`, title: 'Dedicada a mi ex' },
+  ];
+  const [slide, setSlide] = useState(0);
+  // setTimeout(() => (slide === 1 ? setSlide(0) : setSlide(1)), 2000);
 
   return (
     <div className={classes}>
-      <Image src="https://klike.net/uploads/posts/2019-11/1572612050_1.jpg"
+      <Image src={test[slide].src}
       className={styles.background}
       />
+      <div className={styles.description}>
+        <Typography
+          className={styles.title}
+        >{test[slide].title.toUpperCase()}</Typography>
+      </div>
     </div>
   );
 };
